3c3c57a7bebb71939df1c91ce0db01125e2dc1fb
Igorippon <botvunaigor@gmail.com>
2023-07-07 19:57:07 +0300

    add.task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const body = document.querySelector('body');
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
 
+stopBtn.setAttribute('disabled', true);
+let timerId = null;
+
+startBtn.addEventListener("click", () => {
+  timerId = setInterval(() => {
+       const color = getRandomHexColor();
+      body.style.backgroundColor = color;
+      startBtn.setAttribute('disabled', true);
+      stopBtn.removeAttribute('disabled')
+  }, 1000);
+});
+
+stopBtn.addEventListener("click", () => {
+  clearInterval(timerId);
+    startBtn.removeAttribute('disabled');
+    stopBtn.setAttribute('disabled', true);
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
